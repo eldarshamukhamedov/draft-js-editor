ac5d39a5d28f6145f588603e83086d06978db6b7
Eldar Shamukhamedov <shamu1001@gmail.com>
2019-12-26 09:42:02 -0800

    Clean up keyBindingFn

--- src/helpers/keyBindingFn.ts
+++ src/helpers/keyBindingFn.ts
@@ -1,11 +1,29 @@
-import { getDefaultKeyBinding, KeyBindingUtil } from "draft-js";
+import {
+  getDefaultKeyBinding,
+  KeyBindingUtil,
+  DraftEditorCommand,
+} from "draft-js";
 
-const { hasCommandModifier } = KeyBindingUtil;
+type EditorCommand = DraftEditorCommand | string | null;
 
-// Map key event -> key command. Override default key bindings here
-export const keyBindingFn = (e: React.KeyboardEvent) => {
-  if (e.key === "X" && hasCommandModifier(e)) {
-    return "strikethrough";
-  }
+interface KeyOption {
+  key: string;
+  keyCode: number;
+  commandKey: boolean;
+  shiftKey: boolean;
+}
+const getKeyOption = (e: React.KeyboardEvent) => ({
+  key: e.key,
+  keyCode: e.keyCode,
+  command: KeyBindingUtil.hasCommandModifier(e),
+  shift: e.shiftKey,
+});
+
+export const keyBindingFn = (e: React.KeyboardEvent): EditorCommand => {
+  const { key, keyCode, command, shift } = getKeyOption(e);
+  console.debug("[key]", { key, keyCode, command, shift });
+  if (command && keyCode === 192) return "code"; // Cmd + `
+  if (command && shift && keyCode === 88) return "strikethrough"; // Cmd + Shift + X
+
   return getDefaultKeyBinding(e);
 };
