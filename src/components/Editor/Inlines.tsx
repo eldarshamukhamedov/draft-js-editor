63008427080e055bee6f3ca4d4396dc98c453b80
Eldar Shamukhamedov <shamu1001@gmail.com>
2019-12-26 11:18:55 -0800

    WIP refactor inlines

--- src/components/Editor/Inlines.tsx
+++ src/components/Editor/Inlines.tsx
@@ -1,30 +1,44 @@
-import { InlineStyles } from "../../types";
+import { matches } from "lodash";
+import { EditorCommand, InlineStyles, KeyOption } from "../../types";
 
 export interface InlineOption {
   style: InlineStyles;
   label: string;
   icon: string;
+  keySelector: Partial<KeyOption>;
+  editorCommand: EditorCommand;
 }
 
-export const INLINE_OPTIONS: InlineOption[] = [
+export const filterKey = (key: KeyOption, selector: Partial<KeyOption>) =>
+  matches(selector)(key);
+
+export const inlineOptions: InlineOption[] = [
   {
     style: InlineStyles.Bold,
     label: "Bold",
     icon: "format_bold",
+    keySelector: { command: true, keyCode: 66 }, // Cmd + B
+    editorCommand: "bold",
   },
   {
     style: InlineStyles.Italic,
     label: "Italic",
     icon: "format_italic",
+    keySelector: { command: true, keyCode: 73 }, // Cmd + I
+    editorCommand: "italic",
   },
   {
     style: InlineStyles.Underline,
     label: "Underline",
     icon: "format_underline",
+    keySelector: { command: true, keyCode: 85 }, // Cmd + U
+    editorCommand: "underline",
   },
   {
     style: InlineStyles.StrikeThrough,
     label: "Strike-through",
     icon: "format_strikethrough",
+    keySelector: { command: true, shift: true, keyCode: 88 }, // Cmd + Shift + X
+    editorCommand: "strikethrough",
   },
 ];
--- src/helpers/keyBindingFn.ts
+++ src/helpers/keyBindingFn.ts
@@ -1,18 +1,8 @@
-import {
-  getDefaultKeyBinding,
-  KeyBindingUtil,
-  DraftEditorCommand,
-} from "draft-js";
+import { getDefaultKeyBinding, KeyBindingUtil } from "draft-js";
+import { EditorCommand, KeyOption } from "../types";
+import { inlineOptions, filterKey } from "../components/Editor/Inlines";
 
-type EditorCommand = DraftEditorCommand | string | null;
-
-interface KeyOption {
-  key: string;
-  keyCode: number;
-  commandKey: boolean;
-  shiftKey: boolean;
-}
-const getKeyOption = (e: React.KeyboardEvent) => ({
+const getKeyOption = (e: React.KeyboardEvent): KeyOption => ({
   key: e.key,
   keyCode: e.keyCode,
   command: KeyBindingUtil.hasCommandModifier(e),
@@ -20,10 +10,10 @@ const getKeyOption = (e: React.KeyboardEvent) => ({
 });
 
 export const keyBindingFn = (e: React.KeyboardEvent): EditorCommand => {
-  const { key, keyCode, command, shift } = getKeyOption(e);
-  console.debug("[key]", { key, keyCode, command, shift });
-  if (command && keyCode === 192) return "code"; // Cmd + `
-  if (command && shift && keyCode === 88) return "strikethrough"; // Cmd + Shift + X
+  const inlineCommand = inlineOptions
+    .filter(inline => filterKey(getKeyOption(e), inline.keySelector))
+    .map(inline => inline.editorCommand)
+    .pop();
 
-  return getDefaultKeyBinding(e);
+  return inlineCommand || getDefaultKeyBinding(e);
 };
